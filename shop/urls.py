96c0861a511be27f9d3c76ad868e4ed9d186b092
fj <parsafiruzjaei@gmail.com>
2025-09-07 15:42:03 +0330

    'loginLogoutSystemAdded'

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,5 @@
 
-from .views import helloAdmin, about
+from .views import *
 from django.urls import path, include
  
 
@@ -7,5 +7,7 @@ urlpatterns = [
   
     path('', helloAdmin, name="home"),
     path('about/', about, name="about"),
-    
+    path('learn/', learn, name="learn"),
+    path('login/', login_user, name="login"),
+    path('logout/', logout_user, name="logout"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def helloAdmin(request):
     all_products = Product.objects.all()
@@ -9,3 +11,28 @@ def helloAdmin(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def learn(request):
+    return render(request, 'learn.html')
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            login(request, user)
+            messages.success(request,("با موفقیت وارد شدید"))
+            return redirect("home")
+        else:
+            messages.error(request,("ورود ناموفق"))
+            return redirect("login")
+
+    else:
+        return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("شما خارج شدید !"))
+    return redirect("home")
