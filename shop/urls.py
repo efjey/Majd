8e7d2ff05501b33ad35e79129ed1f1d84394fb95
efjey <parsafiruzjaei@gmail.com>
2025-09-11 13:52:28 +0430

    'someBugsFixed'

--- shop/urls.py
+++ shop/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('logout/', views.logout_user, name="logout"),
     path('signup/', views.signup_user, name="signup"),
     path('product/<int:pk>', views.product, name="product"),
+    path('category/<str:cat>', views.category, name="category"),
+
 ]
