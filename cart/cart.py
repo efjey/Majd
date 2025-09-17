ad5c3651fb34378dc2deea2ab9c8f5f1d0c2b41b
fj <parsafiruzjaei@gmail.com>
2025-09-17 13:15:04 +0330

    'userCanSeeTotalOfCart'

--- cart/cart.py
+++ cart/cart.py
@@ -34,6 +34,21 @@ class Cart:
         quantities = self.cart
         return quantities
     
+    def get_total(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        total = 0
+        for key, value in self.cart.items():
+            key = int(key)
+            for product in products:
+                if product.id == key:
+                    if product.is_sale:
+                        total = total + (product.sale_price*value)
+                    else:
+                        total = total + (product.price*value)
+        return total
+                    
+    
     def update(self, product, quantity):
         product_id = str(product)
         product_qty = int(quantity)
@@ -53,3 +68,6 @@ class Cart:
             del self.cart[product_id]
         
         self.session.modified = True
+
+
+    
