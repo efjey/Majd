64ce8b9f874bc35af277740f35481a70c50f47a1
fj <parsafiruzjaei@gmail.com>
2025-09-21 14:38:46 +0330

    'userCanUpdateInf'

--- shop/forms.py
+++ shop/forms.py
@@ -1,6 +1,43 @@
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django import forms
+
+class UpdateUserForm(UserChangeForm):
+    password = None
+    first_name = forms.CharField(
+        label="",
+        max_length=50,
+        widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'نام خود را وارد کنید'})
+
+    )
+
+    last_name = forms.CharField(
+        label="",
+        max_length=50,
+        widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'نام خانوادگی خود را وارد کنید'})
+
+    )
+
+    email = forms.EmailField(
+        
+        label="",
+        widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'ایمییل خود را وارد کنید'})
+
+    )
+
+    username = forms.CharField(
+        label="",
+        max_length=15,
+        widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'نام کاربری خود را وارد کنید'})
+
+    )
+
+    
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
+
+
 
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(
