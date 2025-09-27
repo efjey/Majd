d419f10bdbaa270ec033fcc9e15846108a3f0c30
fj <parsafiruzjaei@gmail.com>
2025-09-27 16:44:20 +0330

    'checkOutPageCreated'

--- payment/urls.py
+++ payment/urls.py
@@ -6,6 +6,7 @@ from django.urls import path
 urlpatterns = [
   
     path('payment_success/', views.payment_success, name="payment_success"),
+    path('checkout/', views.checkout, name="checkout"),
   
     
 
