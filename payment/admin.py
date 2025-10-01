857a40188d4272e4a72e132dcd9cdc5c21a0c65d
fj <parsafiruzjaei@gmail.com>
2025-10-01 14:39:05 +0330

    'dateOfLastUpdateAdded'

--- payment/admin.py
+++ payment/admin.py
@@ -7,9 +7,11 @@ admin.site.register(OrderItem)
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
-    extra = 1
+    extra = 0
+
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = ['date_ordered', 'last_update']
     inlines = [OrderItemInline]
 
